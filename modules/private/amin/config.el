74ad6e145fe78083fc39282c802cdd80884786d0
Amin Bandali <amin@aminb.org>
2017-09-05 23:48:48 -0400

    Add my private module

---
+++ modules/private/amin/config.el
@@ -0,0 +1,155 @@
+;;; private/amin/config.el -*- lexical-binding: t; -*-
+
+(setq auth-sources (list "~/.authinfo.gpg" ))
+
+(map!
+ "C-s" #'swiper
+ "C-S-l" #'recenter
+ "C-x C-b" #'ibuffer
+ [C-S-return] #'recompile
+ :n "gl" #'recenter
+ :n "/" #'swiper
+ ;; "<A-right>" #'right-word
+ ;; "<A-left>" #'left-word
+ ;; "<A-backspace>" #'backward-kill-word
+ ;; "C-=" #'count-words-region
+ (:leader
+   :n "cq" #'fill-paragraph
+   :n "ca" #'auto-fill-mode
+   :n "cc" #'count-words-region
+   ;; :n "tw" #'writeroom-mode
+   )
+ (:leader
+   :desc "dashboard" :n "d" #'+doom-dashboard/open
+   :desc "Centered Window Mode" :n "C" #'centered-window-mode
+   (:desc "Dumb Jump" :prefix "j"
+     :desc "Go" :n "j" #'dumb-jump-go
+     :desc "Back" :n "k" #'dumb-jump-back))
+ ;; :leader :desc "Centered Window Mode" :n "C" #'centered-window-mode
+
+ ;; (:after evil-magit
+ ;;   :map (magit-status-mode-map magit-revision-mode-map)
+ ;;   :n "N" #'magit-svn-popup)
+ )
+
+;; (after! evil-escape
+;;   ;; Change the evil escape sequence to fd instead of jk
+;;   (setq evil-escape-key-sequence "fd"))
+
+(after! dumb-jump
+  (setq dumb-jump-force-searcher 'rg))
+
+(map! :map emmet-mode-keymap
+      :i [C-return] #'emmet-expand-yas)
+
+(def-package! magit-svn
+  :after magit)
+
+(setq TeX-PDF-mode t                  ; use pdflatex instead of latex
+      TeX-engine 'xetex)
+
+;; Support zathura in TeX mode
+(setq TeX-view-program-selection
+      '(((output-dvi style-pstricks)
+         "dvips and gv")
+        (output-dvi "xdvi")
+        (output-pdf "Zathura")
+        (output-html "xdg-open")))
+;; (setq TeX-view-program-list
+;;       '(("zathura"
+;;          ("zathura" (mode-io-correlate "-sync.sh")
+;;           " "
+;;           (mode-io-correlate "%n:1:%t ")
+;;           "%o"))))
+
+(def-package! auctex-latexmk             ; latexmk command for AUCTeX
+  :after latex
+  :config
+  (auctex-latexmk-setup)
+
+  (if (< emacs-major-version 24)
+    (defun file-name-base (&optional filename)
+      "Return the base name of the FILENAME: no directory, no extension.
+FILENAME defaults to `buffer-file-name'."
+      (file-name-sans-extension
+       (file-name-nondirectory (or filename (buffer-file-name))))))
+
+  ;; Recompile with latexmk -xelatex on save
+  (add-hook 'after-save-hook
+            (lambda ()
+              (when (string= major-mode 'latex-mode)
+                (TeX-run-latexmk
+                 "LaTeX"
+                 (format "latexmk -xelatex %s" (buffer-file-name))
+                 (file-name-base (buffer-file-name)))))))
+
+(def-package! ripgrep
+  :commands ripgrep-regexp)
+
+(def-package! projectile-ripgrep
+  :after projectile
+  :commands projectile-ripgrep)
+
+(def-package! visual-fill-column
+  :init
+  (dolist (hook '(prog-mode-hook
+                  text-mode-hook
+                  ;; notmuch-show-hook
+                  ;; haskell-cabal-mode-hook
+                  ))
+    (add-hook hook #'visual-fill-column-mode))
+  :config
+  ;; Center text by default and move the fringes close to the text.
+  (setq-default visual-fill-column-center-text t
+                visual-fill-column-fringes-outside-margins nil
+                ;; take into account the line numbers in Emacs 26
+                visual-fill-column-width (+ fill-column 6))
+  ;; Split windows vertically despite large margins, because Emacs otherwise
+  ;; refuses to vertically split windows with large margins
+  (setq split-window-preferred-function
+        #'visual-fill-column-split-window-sensibly))
+
+(remove-hook 'org-mode-hook #'visual-line-mode)
+
+(setq tramp-default-method "ssh")
+
+;; (after! doom-themes
+  ;; Since Inconsolata doesn't have an italicized variant, use LGC instead
+  ;; (set-face-attribute 'italic nil
+  ;;                     :foreground "#333"
+  ;;                     :family "Inconsolata LGC"
+  ;;                     :height 100);; )
+
+;; (custom-theme-set-faces 'user
+;;  '(italic ((t (:slant italic
+;;                :height 100
+;;                :family "Inconsolata LGC")))))
+
+
+;; (def-package! writeroom-mode
+;;   :commands writeroom-mode)
+
+;; (after! org
+;;   (setq-default org-indent-indentation-per-level 4))
+
+;; (after! shackle
+  ;; (add-to-list 'shackle-rules '("\\`\\*notmuch.*?\\*\\'" :popup t)))
+
+;; (require 'material-theme)
+;; (require 'ample-theme)
+;; (require 'color-theme-sanityinc-tomorrow)
+;; (require 'flatui-theme)
+;; (require 'minimal-theme)
+;; (require 'solarized-theme)
+;; (require 'spacemacs-common)
+;; (require 'eziam-common)
+;; (require 'eziam-light-theme)
+;; (require 'nubox)
+;; (require 'nubox-light-theme)
+;; (require 'zerodark-theme)
+;; (require 'gruvbox)
+(require 'gruvbox-theme)
+(require 'gruvbox-light-hard-theme)
+;; (require 'gruvbox-light-medium-theme)
+;; (require 'gruvbox-dark-medium-theme)
+
---
+++ modules/private/amin/packages.el
@@ -0,0 +1,23 @@
+;; -*- no-byte-compile: t; -*-
+;;; private/amin/packages.el
+
+(package! magit-svn)
+(package! auctex-latexmk)
+;; (package! material-theme)
+;; (package! ample-theme)
+;; (package! color-theme-sanityinc-tomorrow)
+(package! flatui-theme)
+(package! minimal-theme)
+;; (package! solarized-theme)
+(package! spacemacs-theme)
+(package! eziam-theme)
+;; (package! nubox)
+;; (package! zerodark-theme)
+(package! gruvbox-theme)
+
+(package! ag)
+(package! ripgrep)
+(package! projectile-ripgrep)
+
+(package! visual-fill-column)
+;; (package! writeroom-mode)
