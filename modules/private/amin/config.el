cff67c4a255de1b88db5110881e25353e8531ed6
Amin Bandali <amin@aminb.org>
2017-12-26 23:50:44 -0500

    private/amin: move hydras to +hydras

--- modules/private/amin/config.el
+++ modules/private/amin/config.el
@@ -1,5 +1,7 @@
 ;;; private/amin/config.el -*- lexical-binding: t; -*-
 
+(load! +hydras)
+
 ;;
 ;; Global config
 ;;
@@ -265,106 +267,6 @@ to another project."
 
 (def-package! dired+
   :config (diredp-toggle-find-file-reuse-dir 1))
-
-
-;;
-;; ibuffer hydra
-;; from https://github.com/abo-abo/hydra/wiki/Ibuffer
-;;
-
-(def-hydra! hydra-ibuffer-main (:color pink :hint nil)
-  "
- ^Navigation^ | ^Mark^        | ^Actions^         | ^View^       | ^Select^
--^----------^-+-^----^--------+-^-------^---------+-^----^-------+-^------^----------
-  _k_:    ↑   | _m_: mark     | _D_: delete       | _g_: refresh | _q_: quit
- _RET_: visit | _u_: unmark   | _S_: save         | _s_: sort    | _TAB_: toggle
-  _j_:    ↓   | _*_: specific | _a_: all actions  | _/_: filter  | _o_: other window
-            | _t_: toggle   | _._: toggle hydra | _H_: help    |
--^----------^-+-^----^--------+-^-------^---------+-^----^-------+-^------^----------
-"
-  ("j" ibuffer-forward-line)
-  ("RET" ibuffer-visit-buffer :color blue)
-  ("k" ibuffer-backward-line)
-
-  ("m" ibuffer-mark-forward)
-  ("u" ibuffer-unmark-forward)
-  ("*" hydra-ibuffer-mark/body :color blue)
-  ("t" ibuffer-toggle-marks)
-
-  ("D" ibuffer-do-delete)
-  ("S" ibuffer-do-save)
-  ("a" hydra-ibuffer-action/body :color blue)
-  ("." nil "toggle hydra" :color blue)
-
-  ("g" ibuffer-update)
-  ("s" hydra-ibuffer-sort/body :color blue)
-  ("/" hydra-ibuffer-filter/body :color blue)
-  ("H" describe-mode :color blue)
-
-  ("q" quit-window "quit ibuffer" :color blue)
-  ("TAB" ibuffer-toggle-filter-group)
-  ("o" ibuffer-visit-buffer-other-window "other window" :color blue))
-
-(def-hydra! hydra-ibuffer-mark (:color teal :columns 5
-                              :after-exit (hydra-ibuffer-main/body))
-  "Mark"
-  ("*" ibuffer-unmark-all "unmark all")
-  ("M" ibuffer-mark-by-mode "mode")
-  ("m" ibuffer-mark-modified-buffers "modified")
-  ("u" ibuffer-mark-unsaved-buffers "unsaved")
-  ("s" ibuffer-mark-special-buffers "special")
-  ("r" ibuffer-mark-read-only-buffers "read-only")
-  ("/" ibuffer-mark-dired-buffers "dired")
-  ("e" ibuffer-mark-dissociated-buffers "dissociated")
-  ("h" ibuffer-mark-help-buffers "help")
-  ("z" ibuffer-mark-compressed-file-buffers "compressed")
-  ("b" hydra-ibuffer-main/body "back" :color blue))
-
-(def-hydra! hydra-ibuffer-action (:color teal :columns 4
-                                :after-exit
-                                (if (eq major-mode 'ibuffer-mode)
-                                    (hydra-ibuffer-main/body)))
-  "Action"
-  ("A" ibuffer-do-view "view")
-  ("E" ibuffer-do-eval "eval")
-  ("F" ibuffer-do-shell-command-file "shell-command-file")
-  ("I" ibuffer-do-query-replace-regexp "query-replace-regexp")
-  ("H" ibuffer-do-view-other-frame "view-other-frame")
-  ("N" ibuffer-do-shell-command-pipe-replace "shell-cmd-pipe-replace")
-  ("M" ibuffer-do-toggle-modified "toggle-modified")
-  ("O" ibuffer-do-occur "occur")
-  ("P" ibuffer-do-print "print")
-  ("Q" ibuffer-do-query-replace "query-replace")
-  ("R" ibuffer-do-rename-uniquely "rename-uniquely")
-  ("T" ibuffer-do-toggle-read-only "toggle-read-only")
-  ("U" ibuffer-do-replace-regexp "replace-regexp")
-  ("V" ibuffer-do-revert "revert")
-  ("W" ibuffer-do-view-and-eval "view-and-eval")
-  ("X" ibuffer-do-shell-command-pipe "shell-command-pipe")
-  ("b" nil "back"))
-
-(def-hydra! hydra-ibuffer-sort (:color amaranth :columns 3)
-  "Sort"
-  ("i" ibuffer-invert-sorting "invert")
-  ("a" ibuffer-do-sort-by-alphabetic "alphabetic")
-  ("v" ibuffer-do-sort-by-recency "recently used")
-  ("s" ibuffer-do-sort-by-size "size")
-  ("f" ibuffer-do-sort-by-filename/process "filename")
-  ("m" ibuffer-do-sort-by-major-mode "mode")
-  ("b" hydra-ibuffer-main/body "back" :color blue))
-
-(def-hydra! hydra-ibuffer-filter (:color amaranth :columns 4)
-  "Filter"
-  ("m" ibuffer-filter-by-used-mode "mode")
-  ("M" ibuffer-filter-by-derived-mode "derived mode")
-  ("n" ibuffer-filter-by-name "name")
-  ("c" ibuffer-filter-by-content "content")
-  ("e" ibuffer-filter-by-predicate "predicate")
-  ("f" ibuffer-filter-by-filename "filename")
-  (">" ibuffer-filter-by-size-gt "size")
-  ("<" ibuffer-filter-by-size-lt "size")
-  ("/" ibuffer-filter-disable "disable")
-  ("b" hydra-ibuffer-main/body "back" :color blue))
 
 ;; (def-package! zoom-frm
 ;;   :commands (zoom-in/out zoom-in zoom-out))
